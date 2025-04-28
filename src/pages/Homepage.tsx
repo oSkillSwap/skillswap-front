376b284a1d591ed0f2c65ea4e4c224897b4ebeaf
Olivier Renard <renard.oliv@gmail.com>
2025-04-28 11:09:15 +0200

    feat(homepage): implement static content structure - responsive optimization pending

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import { LogIn, Search } from 'lucide-react';
+import { LogIn, Search, Star } from 'lucide-react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import './Homepage.scss';
@@ -33,26 +33,74 @@ function Homepage() {
 
         <section className="homepage-section">
           <h1 className="homepage-section-title">Catégories</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-            expedita aut, porro nostrum aliquam eum vitae distinctio ullam illum
-            consectetur eius nisi asperiores quibusdam, necessitatibus quas nam
-            at deleniti illo!
-          </p>
+          <div className="categories">
+            <div className="categories-card">
+              <img
+                className="categories-card-img"
+                src="img/icons/graduation-cap.svg"
+                alt="Soutien scolaire"
+              />
+              <p className="categories-card-title">Soutien scolaire</p>
+            </div>
+            <div className="categories-card">
+              <img
+                className="categories-card-img"
+                src="img/icons/mouse-pointer-click.svg"
+                alt="Stratégie digitales"
+              />
+              <p className="categories-card-title">Stratégie digitales</p>
+            </div>
+            <div className="categories-card">
+              <img
+                className="categories-card-img"
+                src="img/icons/shirt.svg"
+                alt="Couture et retouches"
+              />
+              <p className="categories-card-title">Couture et retouches</p>
+            </div>
+            <div className="categories-card">
+              <img
+                className="categories-card-img"
+                src="img/icons/scan-face.svg"
+                alt="Beauté et style"
+              />
+              <p className="categories-card-title">Beauté et style</p>
+            </div>
+            <div className="categories-card">
+              <img
+                className="categories-card-img"
+                src="img/icons/gauge.svg"
+                alt="Coaching de vie"
+              />
+              <p className="categories-card-title">Coaching de vie</p>
+            </div>
+            <div className="categories-card">
+              <img
+                className="categories-card-img"
+                src="img/icons/code-xml.svg"
+                alt="Dev-pro"
+              />
+              <p className="categories-card-title">Dev-pro</p>
+            </div>
+          </div>
         </section>
 
-        <section className="homepage-section section1">
-          <img src="img/home-image.jpg" alt="" />
-          <div className="section1-content">
-            <h1>Recherchez, proposez, apprenez</h1>
-            <p>
-              Créez votre profil, décrivez vos savoir-faire, et indiquez ce que
-              vous aimeriez apprendre. Chaque compétence est une monnaie
-              d'échange.
-            </p>
-            <button className="btn btn-default">
-              <LogIn /> Je m'identifie
-            </button>
+        <section className="homepage-section">
+          <div className="homepage-section-textleft">
+            <img src="img/home-image.jpg" alt="" />
+            <div className="homepage-section-textleft-content">
+              <div>
+                <h1>Recherchez, proposez, apprenez</h1>
+                <p>
+                  Créez votre profil, décrivez vos savoir-faire, et indiquez ce
+                  que vous aimeriez apprendre. Chaque compétence est une monnaie
+                  d'échange.
+                </p>
+              </div>
+              <button className="btn btn-default">
+                <LogIn /> Je m'identifie
+              </button>
+            </div>
           </div>
         </section>
 
@@ -62,6 +110,87 @@ function Homepage() {
             <UserCard />
             <UserCard />
             <UserCard />
+          </div>
+        </section>
+
+        <section className="homepage-section">
+          <h1 className="homepage-section-title">Ce qu'ils en pensent</h1>
+          <div className="testimonials">
+            <article className="testimonials-card">
+              <img
+                className="testimonials-card-picture"
+                src="img/avatars/robot1.jpg"
+                alt=""
+              />
+              <div className="testimonials-card-title">
+                <h3>Vraiment génial !</h3>
+                <div className="testimonials-card-grade">
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star" />
+                </div>
+              </div>
+              <p className="testimonials-card-comment">
+                J'ai échangé mes compétences en graphisme contre des cours de
+                guitare, et c'était une expérience incroyable. Simple, humain,
+                et tellement enrichissant !
+              </p>
+              <p className="testimonials-card-info">
+                Username, le 22 avril 2025
+              </p>
+            </article>
+            <article className="testimonials-card">
+              <img
+                className="testimonials-card-picture"
+                src="img/avatars/robot1.jpg"
+                alt=""
+              />
+              <div className="testimonials-card-title">
+                <h3>Vraiment génial !</h3>
+                <div className="testimonials-card-grade">
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star" />
+                </div>
+              </div>
+              <p className="testimonials-card-comment">
+                J'ai échangé mes compétences en graphisme contre des cours de
+                guitare, et c'était une expérience incroyable. Simple, humain,
+                et tellement enrichissant !
+              </p>
+              <p className="testimonials-card-info">
+                Username, le 22 avril 2025
+              </p>
+            </article>
+            <article className="testimonials-card">
+              <img
+                className="testimonials-card-picture"
+                src="img/avatars/robot1.jpg"
+                alt=""
+              />
+              <div className="testimonials-card-title">
+                <h3>Vraiment génial !</h3>
+                <div className="testimonials-card-grade">
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star star-active" />
+                  <Star className="star" />
+                </div>
+              </div>
+              <p className="testimonials-card-comment">
+                J'ai échangé mes compétences en graphisme contre des cours de
+                guitare, et c'était une expérience incroyable. Simple, humain,
+                et tellement enrichissant !
+              </p>
+              <p className="testimonials-card-info">
+                Username, le 22 avril 2025
+              </p>
+            </article>
           </div>
         </section>
       </main>
