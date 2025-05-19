8e99c62d34516ee6cd50b2a5c50d0fa7ca59e426
Olivier Renard <renard.oliv@gmail.com>
2025-05-19 16:42:22 +0200

    Add ErrorPage404 styles for animated character and layout

--- src/pages/static/ErrorPage404.tsx
+++ src/pages/static/ErrorPage404.tsx
@@ -1,10 +1,26 @@
+import { Link } from 'react-router';
 import PageTransition from '../../utils/PageTransition';
+import './ErrorPage404.scss';
+import { Undo2 } from 'lucide-react';
 
 function ErrorPage404() {
   return (
-    <main className="container">
+    <main className="error404 container">
       <section className="content">
-        <h1>Error: 404</h1>
+        <div className="scene">
+          <img className="scene-bg" src="/src/assets/img/404-bg.png" alt="" />
+          <img
+            className="scene-char"
+            src="/src/assets/img/404-character.png"
+            alt=""
+          />
+        </div>
+        <h1>404</h1>
+        <p>Hmmm... Il semblerait que vous vous soyez perdu...</p>
+        <Link className="btn btn-default btn-icon" to="/">
+          <Undo2 />
+          Retourner vers l'accueil
+        </Link>
       </section>
     </main>
   );
