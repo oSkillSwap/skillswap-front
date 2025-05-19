87c590c0c0d83224c37df1af2291bd6edb555dbb
GreyGiver <greygivers@gmail.com>
2025-05-19 09:14:53 +0200

    fix: merge error isLoadding

--- src/components/CategoryCarousel.tsx
+++ src/components/CategoryCarousel.tsx
@@ -11,44 +11,46 @@ import type { LucideIcon } from 'lucide-react';
 const iconMap = LucideIcons as unknown as Record<string, LucideIcon>;
 
 function CategoryCarousel() {
-  const { categories } = useCategories();
+  const { categories, isLoading } = useCategories();
 
   return (
     <>
       <Loader isVisible={isLoading} />
       {!isLoading && (
-    <Swiper
-      className="categories"
-      spaceBetween={32}
-      slidesPerView={2}
-      loop={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-      }}
-      breakpoints={{
-        640: { slidesPerView: 3, spaceBetween: 32 },
-        768: { slidesPerView: 4, spaceBetween: 32 },
-        1024: { slidesPerView: 5, spaceBetween: 32 },
-      }}
-      modules={[Autoplay]}
-    >
-      {categories.map((category: ICategories) => {
-        const Icon = iconMap[category.icon] || LucideIcons.HelpCircle;
+        <Swiper
+          className="categories"
+          spaceBetween={32}
+          slidesPerView={2}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          breakpoints={{
+            640: { slidesPerView: 3, spaceBetween: 32 },
+            768: { slidesPerView: 4, spaceBetween: 32 },
+            1024: { slidesPerView: 5, spaceBetween: 32 },
+          }}
+          modules={[Autoplay]}
+        >
+          {categories.map((category: ICategories) => {
+            const Icon = iconMap[category.icon] || LucideIcons.HelpCircle;
 
-        return (
-          <SwiperSlide
-            className="categories-card"
-            key={category.id}
-            data-id={category.id}
-          >
-            <Icon className="categories-card-icon" size={36} />
-            <p className="categories-card-title">{category.name}</p>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+            return (
+              <SwiperSlide
+                className="categories-card"
+                key={category.id}
+                data-id={category.id}
+              >
+                <Icon className="categories-card-icon" size={36} />
+                <p className="categories-card-title">{category.name}</p>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      )}
+    </>
   );
 }
 
